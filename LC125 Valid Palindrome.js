13620823b7582d9d1462b80913c85ac51393316e
AshleyCheny <AshleyCheny@gmail.com>
2017-09-12 22:19:51 -0700

    added comment for valid palindrome

--- LC125 Valid Palindrome.js
+++ LC125 Valid Palindrome.js
@@ -3,17 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+
   //how to remove punctuation, spaces and symbols in javascript
   s = s.replace(/[^A-Za-z0-9]/g,"").replace(' ', '').toLowerCase();
-  
+
   //convert a string to an array and reverse the array, then convert the array to string using join() without comma
   var reversedStr = s.split("").reverse().join('');
-  
+
+  //compare the original string and reversed str
   if(reversedStr === s){
     return true;
   } else {
     return false;
   }
-  
+
 };
